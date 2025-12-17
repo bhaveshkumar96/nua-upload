9492daba4ed1ae74c9f362854588a5af650d9a66
Bhavesh Kumar <110034571+bhaveshkumar96@users.noreply.github.com>
2025-12-17 12:26:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,14 +15,8 @@ const allowedOrigins = [
   "https://nua-upload.onrender.com",
 ];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors());
+
 
 app.options("*", cors());
 app.get("/", (req, res) => {
